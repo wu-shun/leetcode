45005be2d677dede8bb981741469128a7c6715c1
Sinosun <wu_shun@126.com>
2022-08-22 17:46:08 +0800

    添加忽略文件

---
+++ src/main/java/com/ws/ThreadTest.java
@@ -0,0 +1,9 @@
+package com.ws;
+
+/**
+ * @Copyright: Copyright (c) 兆日科技股份有限公司
+ * @Date 2022/8/22 17:24
+ * @Des
+ */
+public class ThreadTest {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * @Copyright: Copyright (c) 兆日科技股份有限公司
+ * @Date 2022/8/22 17:23
+ * @Des
+ */
+public class Test {
+}
